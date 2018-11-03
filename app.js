7178bc2c6a9f72c35163519ddd0b2ad15e5b6a7e
Sanjay Srinivas <srinivassanjay@gmail.com>
2018-11-03 17:47:42 +1100

    Testing Router

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 let express = require("express");
+const router = express.Router();
 let port = process.env.PORT || 3000;
 let app = express();
 let moment = require('moment');
 const name = process.env.MY_NAME || `[Name not set in environment variables]`
+
+router.get("/", function (req, res) {
+
+    res.setHeader('content-type', 'application/json');
+    res.send(JSON.stringify({
+        message: `[Using Router] Root page. Hello ${name}.`
+    }));
+});
 
 app.get("/", function (req, res) {
 
@@ -16,9 +25,11 @@ app.post("/subject", (req, res) => {
     res.send(JSON.stringify({
         subject: `dummy`,
         transaction_id: req.query[`transaction_id`],
-        created_date: $moment().format()
+        created_date: `${moment().format()}`
     }));
 });
+
+app.use('/routes', router) // 4
 
 app.listen(port, function () {
     console.log(`Example app listening on port ${port}!`);
