7f9f1199148f853180d3f7617acc2f8477a76147
Sanjay Srinivas <srinivassanjay@gmail.com>
2018-11-04 13:07:24 +1100

    Port earlier file to this...

--- src/server.js
+++ src/server.js
@@ -1,26 +1,65 @@
 'use strict';
-const express = require('express');
-const serverless = require('serverless-http');
-const app = express();
-const bodyParser = require('body-parser');
 
+const express = require("express");
 const router = express.Router();
-router.get('/', (req, res) => {
-    res.writeHead(200, {
-        'Content-Type': 'text/html'
-    });
-    res.write('<h1>Hello from Express.js!</h1>');
-    res.end();
+let port = process.env.PORT || 3000;
+let app = express();
+let moment = require('moment');
+let bodyparser = require('body-parser')
+const addRequestId = require('express-request-id')();
+const serverless = require('serverless-http');
+const morgan = require('morgan')
+var winston = require('./config/winston');
+
+
+const name = process.env.MY_NAME || `[Name not set in environment variables]`
+
+// Example using Router - Gets resolved to /routes because of how it's mounted below
+router.get("/", function (req, res) {
+    winston.info('Hello again distributed logs');
+    res.setHeader('content-type', 'application/json');
+    res.send(JSON.stringify({
+        message: `[Using Router] Root page. Hello ${name}.`
+    }));
 });
-router.get('/another', (req, res) => res.json({
-    route: req.originalUrl
-}));
-router.post('/', (req, res) => res.json({
-    postBody: req.body
-}));
 
-app.use(bodyParser.json());
-app.use('/.netlify/functions/server', router); // path must route to lambda
+app.get("/", function (req, res) {
+    res.setHeader('content-type', 'application/json');
+    res.send(JSON.stringify({
+        message: `Root page. Hello ${name}.`
+    }));
+});
 
-module.exports = app;
+app.post("/subject", (req, res) => {
+    res.send(JSON.stringify({
+        subject: `dummy`,
+        transaction_id: req.query[`transaction_id`],
+        created_date: `${moment().format()}`
+    }));
+});
+
+// Example using route parameters also request parameters! confusing sometimes...
+// Will resolve to, say, http://localhost:3000/users/sanju/vices/wine
+app.get("/users/:userid/vices/:viceid", (req, res) => {
+    console.log(`Request params: ${req.params}`);
+    res.send(JSON.stringify({
+        user: `${req.params.userid}`,
+        vice: `${req.params.viceid}`
+    }))
+});
+
+app.use(addRequestId);
+app.use(morgan('combined', {
+    stream: winston.stream
+}));
+app.use('/routes', router);
+app.use(bodyparser.json())
+app.use('/.netlify/functions/server', router);
+
+app.listen(port, function () {
+    console.log(`Example app listening on port ${port}!`);
+});
+
+
+module.exports = app
 module.exports.handler = serverless(app);
